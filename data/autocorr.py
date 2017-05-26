5f9ec4f38ccf9cf92ed22a7daabb236c0b46bad0
florencia lazzari <flazzari@ws22.labo5>
2017-05-26 16:16:07 -0300

    trabajando en autocorr

--- data/autocorr.py
+++ data/autocorr.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 datos = np.loadtxt("datos_magnet_energy.csv", delimiter = ',', skiprows=1)
 
 n_iter = 20000 # cantidad de iteraciones para cada temperatura (de ising.c)
-tiempo_term = 10000 #estimado del tiempo de termalizacion
+tiempo_term = 10000 #estimado del tiempo de termalizacion (a ojo)
 range_temp = 10 #cantidad de temperaturas
-temperatura = np.arange(0.4,4.4,0,4)
+temperatura = np.arange(0.4,4.4,0.4)
 
 #%% Organizar datos
 
@@ -23,15 +23,37 @@ for i in range(range_temp):
     t[:,i] = datos[tiempo_term + i*n_iter : (i+1)*n_iter, 0]
     magnet[:,i] = datos[tiempo_term + i*n_iter : (i+1)*n_iter,1]
     energy[:,i] = datos[tiempo_term + i*n_iter : (i+1)*n_iter,2]
-    # estos dos son para la autocorrelacion
+    # estos dos son para la autocorrelacion (resto el offset)
     m[:,i] = magnet[:,i] - np.mean(magnet[:,i])
     e[:,i] = energy[:,i] - np.mean(energy[:,i])
 
 #%% Graficar m y e
 
-temp = 6 # el indice corresponde a la temp T = 0.4*(i+1) (entre 0.4 y 4)
-
 plt.figure()
+
+for i in range(10):
+    plt.subplot(1,2,1)
+    plt.plot(datos[i*n_iter : (i+1)*n_iter, 0], datos[i*n_iter : (i+1)*n_iter,1], label = 'T = {:.2f}'.format(temperatura[i]))
+
+    plt.subplot(1,2,2)
+    plt.plot(datos[i*n_iter : (i+1)*n_iter,0], datos[i*n_iter : (i+1)*n_iter,2], label = 'T = {:.2f}'.format(temperatura[i]))
+
+plt.subplot(1,2,1)
+plt.grid(True)
+plt.xlabel('Tiempo')
+plt.ylabel('magnetizacion')
+plt.legend()
+
+plt.subplot(1,2,2)
+plt.grid(True)
+plt.xlabel('Tiempo')
+plt.ylabel('energia')
+plt.legend()
+plt.title('Magnetizacion y energia en el tiempo')
+plt.show()
+
+#%% Graficar Fluctuaciones
+
 for temp in range(10):
     plt.subplot(1,2,1)
     plt.plot(t[:,temp], m[:,temp], label = 'T = {:.2f}'.format(temperatura[temp]))
@@ -54,15 +76,23 @@ plt.title('Fluctuaciones de magnetizacion y energia en el tiempo')
 plt.show()
 
 #%% Autocorrelacion
-tiempo_auto = int((n_iter-tiempo_term)/2) # asegurarme de que sea par?
+
+N = 1000
+tiempo_auto = int(n_iter - tiempo_term - 2*N) # asegurarme de que sea par?
 t_rho = np.arange(tiempo_auto)
 rho = np.zeros((tiempo_auto, range_temp))
+
+#
+# def autocorr(x):
+#     result = np.correlate(x, x, mode='full')
+#     return result[result.size/2:]
+
 
 #por ahora para Magnetizacion
 for i in range(range_temp):
     for k in range(1,tiempo_auto):
         # el indice falopa es para evitar cosas como m[:0]
-        rho[k-1,i] = np.mean(m[:k,i]*m[k:2*k,i])/np.mean(m[:,i]**2)
+        rho[k-1,i] = np.mean(m[:N,i]*m[k:N+k,i])/np.mean(m[:N+k,i]**2) # ***revisar biennnnn!!!!
 
 #%% Graficar autocorr
 
@@ -75,3 +105,10 @@ plt.grid(True)
 plt.xlabel('Tiempo')
 plt.ylabel('Autocorrelacion (mag)')
 plt.show()
+
+
+# tiempo_descorr = 15000 # ver de donde sale
+# mag_posta = magnet[tiempo_descorr - tiempo_term :,:]
+# en_posta = energy[tiempo_descorr - tiempo_term :,:]
+# mag_avg = np.mean(mag_posta,0)
+# en_avg = np.mean(en_posta,0)
