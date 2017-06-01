4d980fc220abe601183a3037a9b46f838605e20e
martin <martincarusso@gmail.com>
2017-06-01 15:02:33 -0300

    ahora hago un sampleo espaciado por el t_corr, en otro archivo. Otro script de python hace los graficos, parecen dar bien

---
+++ data/promedios.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+samples = np.loadtxt("datos_sampleo_n32_1k.csv", delimiter = ',', skiprows=1)
+
+n_iter = 1000
+range_temp = 10 #cantidad de temperaturas
+temperatura = np.arange(0.4,4.4,0.4)
+
+#%% Obtener mag y en
+
+tiempo = np.arange(n_iter)
+magnet = np.zeros((n_iter, range_temp))
+energy = np.zeros((n_iter, range_temp))
+
+for i in range(range_temp):
+    magnet[:,i] = samples[i*n_iter : (i+1)*n_iter,1]
+    energy[:,i] = samples[i*n_iter : (i+1)*n_iter,2]
+
+mag_avg = np.abs(np.mean(magnet,0))
+en_avg = np.mean(energy,0)
+
+#%% Graficar
+
+plt.figure()
+plt.subplot(2,1,1)
+plt.plot(temperatura, mag_avg)
+plt.grid(True)
+plt.xlabel('Temperatura')
+plt.ylabel('magnetizacion')
+plt.title('Magnetizacion en funcion de la temperatura')
+#plt.legend()
+
+plt.subplot(2,1,2)
+plt.plot(temperatura, en_avg)
+plt.grid(True)
+plt.xlabel('Temperatura')
+plt.ylabel('energia')
+#plt.legend()
+plt.title('Energia en funcion de la temperatura')
+
+plt.tight_layout()
+plt.show()
--- src/ising.c
+++ src/ising.c
@@ -6,21 +6,23 @@
 
 
 int main(int argc, char **argv) {
-  /*FILE *fdat;
-  fdat = fopen("datos_magnet_energy_n32_300k_B00001.csv", "w");
-  fprintf(fdat, "i    M    E    T\n");*/
-  int n = 3;
+  FILE *fdat;
+  fdat = fopen("datos_sampleo_n32_1k.csv", "w");
+  fprintf(fdat, "i    M    E    T\n");
+  int n = 32;
   int *lattice = malloc(n * n * sizeof(int));
   double *list = malloc(5* sizeof(double));
   float prob = 0.5;
   float T = 0, J = 1, B = 0;
-  int niter = 5;
+  int n_term = 15000;
+  int n_corr = 15000;
+  int n_iter = 1000*n_corr;
   srand(time(NULL));
 
   for (int j=0; j < 10; j++){
 
     T = 0.4 + (4.0/10)*j;
-    B = 0.0001;
+    //B = 0.0001;
     fill_lattice(lattice, n, prob);
     list_exp(list, T, J, B);
 
@@ -28,17 +30,31 @@ int main(int argc, char **argv) {
     float M = magnet(lattice, n);
     float* p_e = &E;
     float* p_m = &M;
-    float* energy_array = malloc(niter * sizeof(float)); // aca voy a guardar los valores de energia
-    float* magnet_array = malloc(niter * sizeof(float)); // aca voy a guardar los valores de magnetizacon
+    float* energy_array = malloc(n_iter * sizeof(float)); // aca voy a guardar los valores de energia
+    float* magnet_array = malloc(n_iter * sizeof(float)); // aca voy a guardar los valores de magnetizacon
 
-    for (int i = 0; i < niter; i++) {
+    // ciclo de termalizacion
+    for (int i = 0; i < n_term; i++) {
       metropolis(lattice, n, T, J, B, p_e, p_m, list);
-       print_lattice(lattice, n);
+      // print_lattice(lattice, n);
       energy_array[i] = *p_e;
       magnet_array[i] = *p_m;
       // printf("ARRAY MAG = %f\n", magnet_array[i]);
-      printf("ARRAY ENERGIA = %f\n", energy_array[i]);
+      //printf("ARRAY ENERGIA = %f\n", energy_array[i]);
   	  //fprintf(fdat, "%i,%3.6g,%3.3g,%3.3g \n",i, magnet_array[i], energy_array[i],T);
+    }
+    // ciclo de sampleo, toma datos solo cada tiempo de descorrelacion
+    for (int i = 0; i < n_iter; i++) {
+      metropolis(lattice, n, T, J, B, p_e, p_m, list);
+      // print_lattice(lattice, n);
+      energy_array[i] = *p_e;
+      magnet_array[i] = *p_m;
+      // printf("ARRAY MAG = %f\n", magnet_array[i]);
+      //printf("ARRAY ENERGIA = %f\n", energy_array[i]);
+      if (i%n_corr == 0){
+        fprintf(fdat, "%i,%3.6g,%3.3g,%3.3g \n",i, magnet_array[i], energy_array[i],T);
+        printf("dato tomado para T = %.2f, i = %i de %i\n", T, i/n_corr, n_iter/n_corr);
+      }
     }
   }
   return 0;
